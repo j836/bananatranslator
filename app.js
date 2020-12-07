36c09f08961a98ab0cc56821f704630998001d48
Prajwal Vishal Jain <jainprajwal123@gmail.com>
2020-12-07 21:03:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const txtinput = document.querySelector('.txt-input');
+const translatebtn = document.querySelector('.translate-btn');
+console.log(translatebtn)
+const txtoutput = document.querySelector('.txt-output')
+console.log(txtoutput);
+
+
+
+
+
+const serverurl = "https://api.funtranslations.com/translate/dolan.json?text=";
+
+function generateURL(text) {
+    return serverurl + text;
+}
+
+
+function clickHandler() {
+    //  alert('Here we go');
+    if(txtinput.value === undefined || txtinput.value === "") {
+        window.alert('Empty input!!Please enter text to get its translation');
+    }
+    fetch(generateURL(txtinput.value))
+    .then(response => response.json())
+    .then(json => {txtoutput.innerText = json.contents.translated});
+}
+translatebtn.addEventListener('click', clickHandler);
+
